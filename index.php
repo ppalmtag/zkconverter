05b4ce8ac37ff4aba97141579a21163a625c1ff8
Philipp Palmtag <ppalmtag@users.noreply.github.com>
2018-03-21 14:22:33 +0100

    Added WEG 26 conversion
    Added WEG 26 conversion, updated Bootstrap

--- index.php
+++ index.php
@@ -3,13 +3,15 @@ if (isset($_POST['hex'])) {
     $hex_code = filter_var($_POST['hex'], FILTER_SANITIZE_STRING);
 
     if (preg_match('/[[:xdigit:]]{10}/', $hex_code) === 1) {
-        $result = '';
-        array_map(function($val) use (&$result) {
+        $zk = '';
+        array_map(function($val) use (&$zk) {
             $binary = strrev(str_pad(base_convert($val, 16, 2), 8, '0', STR_PAD_LEFT));
-            array_map(function($bin) use (&$result) {
-                $result .= str_pad(base_convert($bin, 2, 10), 2, '0', STR_PAD_LEFT);
+            array_map(function($bin) use (&$zk) {
+                $zk .= str_pad(base_convert($bin, 2, 10), 2, '0', STR_PAD_LEFT);
             }, str_split($binary, 4));
         }, str_split($hex_code, 2));
+
+        $weg_26 = str_pad(base_convert(substr($_POST['hex'], -6, 2), 16, 10), 3, '0', STR_PAD_LEFT) . str_pad(base_convert(substr($_POST['hex'], -4), 16, 10), 5, '0', STR_PAD_LEFT);
     } else {
         $code_invalid = true;
     }
@@ -23,13 +25,13 @@ if (isset($_POST['hex'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6tvslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
         <div class="container-fluid">
             <div class="row justify-content-md-center mt-3">
                 <div class="col col-lg-4">
-                    <h1>ZK-Code Converter</h1>
+                    <h1>ZK-Code & WEG 26 Converter</h1>
 
                     <form method="POST">
                         <div class="form-group">
@@ -42,19 +44,33 @@ if (isset($_POST['hex'])) {
                         </div>
                         <button type="submit" class="btn btn-primary">Convert</button>
                     </form>
-                    <?php if (isset($result)): ?>
-                        <blockquote class="blockquote text-center">
-                            <p class="mb-0"><?php echo $result; ?></p>
-                            <footer class="blockquote-footer" style="font-size: 60%;">as 20 digit decimal number</footer>
-                        </blockquote>
+                    <?php if (isset($zk) && isset($weg_26)): ?>
+                        <div class="row justify-content-md-center mt-3">
+                            <div class="col col-2">
+                                <p>ZK-Code</p>
+                            </div>
+                            <div class="col col-4">
+                                <p class="mb-0"><?php echo $zk; ?></p>
+                                <footer class="blockquote-footer" style="font-size: 60%;">as 20 digit decimal number</footer>
+                            </div>
+                        </div>
+                        <div class="row justify-content-md-center">
+                            <div class="col-2">
+                                <p>WEG 26</p>
+                            </div>
+                            <div class="col-4">
+                                <p class="mb-0"> <?php echo $weg_26; ?></p>
+                                <footer class="blockquote-footer" style="font-size: 60%;">as 8 digit decimal number</footer>
+                            </div>
+                        </div>
+
                     <?php endif; ?>
                 </div>
             </div>
         </div>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/41nzFpr53nxSSGLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    </body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
